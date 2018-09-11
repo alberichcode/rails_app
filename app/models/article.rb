f26e3da1f41654e39baa707f3d65090d919e367c
alberichcode <al.gp123@gmail.com>
2018-09-10 20:43:44 -0400

    create articles table

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: {minimun: 3, maximun: 10}
+  validates :description, presence:true, length {minimun: 10, maximun: 50}
+  
+end
