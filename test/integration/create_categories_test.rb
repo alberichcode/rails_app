cf75ca16bf7e95d35baf74f0c06c90fcd899259b
alberichcode <al.gp123@gmail.com>
2018-11-11 20:29:39 -0500

    adding admin restrictions to categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-  test " get new category form and create category" do
+  test "get new category form and create category" do
     get new_category_path
-    assert_template "categories/new"
+    assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
       post_via_redirect categories_path, category: {name: "sports"}
     end
